611385b972f104bc0daf3e5201bc2267e49fd86d
陈智鹏 <112376327+cuzperf@users.noreply.github.com>
2023-11-05 01:12:28 +0800

    refine (#19)

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,4 +1,15 @@
 option(BUILD_32BIT "Build for 32 bit" OFF)
+
+if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+option(COVERAGE "Use gcov" ON)
+else()
 option(COVERAGE "Use gcov" OFF)
+endif()
+
+if(CMAKE_SYSTEM_NAME MATCHES "Windows")
 option(DOXYGEN "Build Doxygen" OFF)
+else()
+option(DOXYGEN "Build Doxygen" ON)
+endif(s)
+
 option(VERBOSE "Verbose message in cmake" OFF)
